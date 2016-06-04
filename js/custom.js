4a5cbf38596ddba976a5803d55ef697a03ae4a64
Jaemyoun Lee <Jaemyoun Lee>
2016-06-05 02:02:29 +0900

    roll back custom js

--- js/custom.js
+++ js/custom.js
@@ -94,10 +94,6 @@ $(window).load(function() {
   $('#status').fadeOut();
   $('#preloader').delay(350).fadeOut('slow');
 
-  //tour trigger
-  Guideline.setCurrentPage('tour');
-  Guideline.getGuide('welcome').start();
-
   //modal trigger
   $('.modal').bigmodal('hide');
 
@@ -109,37 +105,5 @@ $(window).load(function() {
   $('a[href="#"]').click(function() {
     return false;
   });
-
-  //contact form
-  $(document).ready(function() {
-    var options = {
-      target: '.message .alert',
-      beforeSubmit: showRequest,
-      success: showResponse
-    };
-    $('#contactForm').ajaxForm(options);
-  });
-
-  function showRequest(formData, jqForm, options) {
-    var queryString = $.param(formData);
-    return true;
-  }
-  function showResponse(responseText, statusText)  {
-  }
-  $.fn.clearForm = function() {
-    return this.each(function() {
-      var type = this.type, tag = this.tagName.toLowerCase();
-      if (tag === 'form') {
-        $(':input', this).clearForm();
-      }
-      if (type === 'text' || type === 'password' || tag === 'textarea') {
-        this.value = '';
-      } else if (type === 'checkbox' || type === 'radio') {
-        this.checked = false;
-      } else if (tag === 'select') {
-        this.selectedIndex = -1;
-      }
-    });
-  };
 
 });
